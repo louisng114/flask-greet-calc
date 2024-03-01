3045653692de9100252b0110eda220c3ffec5131
louisng114 <louisng114@hotmail.com>
2024-02-29 22:44:06 -0800

    complete exercise

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,43 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.route("/add")
+def add():
+    """add query inputs"""
+    a = request.args["a"]
+    b = request.args["b"]
+    return str(operations.add(float(a),float(b)))
+
+@app.route("/sub")
+def sub():
+    """subtract query inputs"""
+    a = request.args["a"]
+    b = request.args["b"]
+    return str(operations.sub(float(a),float(b)))
+
+@app.route("/mult")
+def mult():
+    """multiply query inputs"""
+    a = request.args["a"]
+    b = request.args["b"]
+    return str(operations.mult(float(a),float(b)))
+
+@app.route("/div")
+def div():
+    """divide query inputs"""
+    a = request.args["a"]
+    b = request.args["b"]
+    return str(operations.div(float(a),float(b)))
+
+@app.route("/math/<op>")
+def math_op(op):
+    """apply operation on query inputs"""
+    a = request.args["a"]
+    b = request.args["b"]
+    ops = {"add": operations.add,
+           "sub": operations.sub,
+           "mult": operations.mult,
+           "div":operations.div}
+    return str(ops[op](float(a), float(b)))
--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome():
+    """Display welcome message"""
+    return "welcome"
+
+@app.route("/welcome/home")
+def welcome_home():
+    """Display welcome home message"""
+    return "welcome home"
+
+@app.route("/welcome/back")
+def welcome_back():
+    """Display welcome back message"""
+    return "welcome back"
